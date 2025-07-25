7dd397b8c63a3cd7019302d027983192b40b85a4
Luke Zhang <lukezha@amazon.com>
2025-07-25 12:45:32 -0700

    enable aws sdk v2 auto upgrade script.

--- zbuild.sh
+++ zbuild.sh
@@ -3,9 +3,23 @@
 set -e
 set -x
 
+# ./gradlew build -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=2096m"
 
-./gradlew assemble
+# ./gradlew clean build -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=2096m"
 
-./gradlew publishToMavenLocal
+# ./gradlew assemble
+
+# ./gradlew publishToMavenLocal
 
 
+# Mac
+/Users/lukezha/.gradle/wrapper/dists/gradle-7.4.2-bin/48ivgl02cpt2ed3fh9dbalvx8/gradle-7.4.2/bin/gradle \
+     -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=8000m" \
+    rewriteRun --init-script ./init.gradle \
+    -Drewrite.activeRecipes=software.amazon.awssdk.v2migration.AwsSdkJavaV1ToV2
+
+# Linux
+# /home/lukezha/.gradle/wrapper/dists/gradle-7.4.2-bin/48ivgl02cpt2ed3fh9dbalvx8/gradle-7.4.2 \
+#     -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=8000m" \
+#     rewriteRun --init-script ./init.gradle \
+#     -Drewrite.activeRecipes=software.amazon.awssdk.v2migration.AwsSdkJavaV1ToV2
