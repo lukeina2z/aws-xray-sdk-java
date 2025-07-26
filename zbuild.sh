f4b1a2701637aec5e3a5acd0b85e936cae150026
Luke Zhang <lukezha@amazon.com>
2025-07-26 07:10:09 -0700

    update 2.18.99 build script.

--- zbuild.sh
+++ zbuild.sh
@@ -5,18 +5,18 @@ set -x
 
 # ./gradlew build -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=2096m"
 
-# ./gradlew clean build -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=2096m"
+./gradlew clean build -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=2096m"
 
 # ./gradlew assemble
 
-# ./gradlew publishToMavenLocal
+./gradlew publishToMavenLocal
 
 
 # Mac
-/Users/lukezha/.gradle/wrapper/dists/gradle-7.4.2-bin/48ivgl02cpt2ed3fh9dbalvx8/gradle-7.4.2/bin/gradle \
-     -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=8000m" \
-    rewriteRun --init-script ./init.gradle \
-    -Drewrite.activeRecipes=software.amazon.awssdk.v2migration.AwsSdkJavaV1ToV2
+# /Users/lukezha/.gradle/wrapper/dists/gradle-7.4.2-bin/48ivgl02cpt2ed3fh9dbalvx8/gradle-7.4.2/bin/gradle \
+#      -Dorg.gradle.jvmargs="-XX:MaxMetaspaceSize=8000m" \
+#     rewriteRun --init-script ./init.gradle \
+#     -Drewrite.activeRecipes=software.amazon.awssdk.v2migration.AwsSdkJavaV1ToV2
 
 # Linux
 # /home/lukezha/.gradle/wrapper/dists/gradle-7.4.2-bin/48ivgl02cpt2ed3fh9dbalvx8/gradle-7.4.2 \
